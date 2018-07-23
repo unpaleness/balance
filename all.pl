4a5364da774b18794bc2087c49411254eece14dd
unpaleness <generalissimusunpaleness@gmail.com>
2018-07-24 01:56:53 +0300

    Third script to show all records, fixes

---
+++ all.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use SCS::DataBase;
+
+my $color_positive = ' bgcolor="#88ff88"';
+my $color_negative = ' bgcolor="#ff8888"';
+my $color_zero = ' bgcolor="#ffff88"';
+
+my $base = SCS::DataBase->connect();
+my $query = $base->prepare( 'SELECT DISTINCT storage FROM records ORDER BY 1' );
+$query->execute();
+my @storages = ();
+my $diffs = {};
+my $totals = {};
+while ( my $row = $query->fetchrow_hashref() ) {
+    push @storages, $row->{storage};
+    $diffs->{ $row->{storage} } = 0;
+    $totals->{ $row->{storage} } = 0;
+}
+$diffs->{total} = 0;
+$totals->{total} = 0;
+
+$query = $base->prepare( qq{
+    SELECT r.date AS period, r.type, r.title, r.storage, r.value
+    FROM records AS r
+    ORDER BY date, id
+} );
+$query->execute();
+my $table = '<table>';
+my @titles = ( 'period', 'type', 'title', @storages, 'diff', 'total' );
+my $data = {};
+while ( my $row = $query->fetchrow_hashref() ) {
+    my $period  = $row->{period};
+    my $type    = $row->{type};
+    my $title   = $row->{title};
+    my $storage = $row->{storage};
+    my $value   = $row->{value};
+    $data->{$period} = {} if !defined $data->{$period};
+    $data->{$period}->{$type} = {} if !defined $data->{$period}->{$type};
+    $data->{$period}->{$type}->{$title} = {} if !defined $data->{$period}->{$type}->{$title};
+    $data->{$period}->{$type}->{$title}->{total} = 0 if !defined $data->{$period}->{$type}->{$title}->{total};
+    $data->{$period}->{$type}->{$title}->{$storage} = 0 if !defined $data->{$period}->{$type}->{$title}->{$storage};
+    $data->{$period}->{$type}->{$title}->{$storage} += $value;
+    $totals->{$storage} += $value;
+    $totals->{total} += $value;
+    $data->{$period}->{$type}->{$title}->{total} += $value;
+}
+
+my $head = '';
+foreach my $title ( @titles ) {
+    $head .= "<th>$title</th>";
+}
+$table .= "<tr>$head</tr>";
+
+my $table_row = '<td><b>Total</b></td><td></td><td></td>';
+foreach my $storage ( @storages ) {
+    my $val = $totals->{$storage};
+    my $color = $val > 0 ? $color_positive : $val < 0 ? $color_negative : '';
+    $table_row .= '<td' . $color . '><b>' . sprintf("%.2f", $val) . '</b></td>';
+}
+my $val = $totals->{total};
+my $color = $val > 0 ? $color_positive : $val < 0 ? $color_negative : '';
+$table_row .= '<td></td><td' . $color . '><b>' . sprintf("%.2f", $val) . '</b></td';
+$table .= "<tr>$table_row</tr>";
+
+foreach my $period ( sort keys %{ $data } ) {
+    foreach my $type ( sort keys %{ $data->{$period} } ) {
+        foreach my $title ( sort keys %{ $data->{$period}->{$type} } ) {
+            my $table_row .= "<td>$period</td><td>$type</td><td>$title</td>";
+            my $record = $data->{$period}->{$type}->{$title};
+            my $diff = 0;
+            foreach my $storage ( @storages ) {
+                if ( defined $record->{$storage} ) {
+                    my $val = $record->{$storage};
+                    my $color = $val > 0 ? $color_positive : $val < 0 ? $color_negative : '';
+                    $table_row .= '<td' . $color . '>' . sprintf("%.2f", $val) . '</td>';
+                    $diff += $record->{$storage};
+                    $diffs->{$storage} += $record->{$storage};
+                } else {
+                    $table_row .= '<td></td>';
+                }
+            }
+            $diffs->{total} += $record->{total};
+            my $color = $diff > 0 ? $color_positive : $diff < 0 ? $color_negative : '';
+            $table_row .= '<td' . $color . '><b>' . ( $diff ? sprintf("%.2f", $diff) : '' ) . '</b></td>';
+            my $val = $diffs->{total};
+            $color = $val > 0 ? $color_positive : $val < 0 ? $color_negative : '';
+            $table_row .= '<td' . $color . '><b>' . sprintf("%.2f", $val) . '</b></td>';
+            $table .= "<tr>$table_row</tr>";
+        }
+    }
+}
+
+$table .= '</table>';
+
+my $sample = qq{
+<!DOCTYPE html>
+<html>
+    <head><title>Balance</title></head>
+    <body>
+        <h2>Detailed balance</h2>
+        <div><font size="1">$table</font></div>
+    </body>
+</html>
+};
+
+print "Content-Type: text/html\n";
+print $sample;
+
+1;
+
--- detailed.pl
+++ detailed.pl
@@ -7,13 +7,13 @@ use SCS::DataBase;
 
 my $base = SCS::DataBase->connect();
 my $query = $base->prepare( qq{
-	SELECT
-		DATE_FORMAT(r.date, '%Y-%m') AS "period",
-		r.title,
-		ROUND(SUM(r.value), 2) AS diff
-	FROM records AS r
-	GROUP BY 1, 2
-	ORDER BY 1, 2
+    SELECT
+        DATE_FORMAT(r.date, '%Y-%m') AS "period",
+        r.title,
+        SUM(r.value) AS diff
+    FROM records AS r
+    GROUP BY 1, 2
+    ORDER BY 1, 2
 } );
 $query->execute();
 my $table = '<table>';
@@ -22,38 +22,39 @@ my @titles = ();
 my $total = {};
 my $data = {};
 while ( my $row = $query->fetchrow_hashref() ) {
-	$data->{ $row->{period} }->{ $row->{title} } = $row->{diff};
-	$titles->{ $row->{title} } = 1;
+    $data->{ $row->{period} }->{ $row->{title} } = $row->{diff};
+    $titles->{ $row->{title} } = 1;
 }
 my $head = '<th>Period</th>';
 foreach my $title ( sort keys %{ $titles } ) {
-	push @titles, $title;
-	$head .= "<th>$title</th>";
+    push @titles, $title;
+    $head .= "<th>$title</th>";
 }
 $head .= "<th>Total</th>";
 $table .= "<tr>$head</tr>";
 foreach my $period ( sort keys %{ $data } ) {
-	my $table_row = "<td>$period</td>";
-	my $sum = 0;
-	foreach my $title ( @titles ) {
-		if ( $data->{ $period }->{ $title } ) {
-			$total->{ $title } += $data->{ $period }->{ $title };
-			$sum += $data->{ $period }->{ $title };
-		}
-		my $diff = $data->{ $period }->{ $title } || '';
-		$table_row .= "<td>$diff</td>";
-	}
-	$sum = sprintf("%.2f", $sum);
-	$table_row .= "<td><b>$sum</b></td>";
-	$table .= "<tr>$table_row</tr>";
+    my $table_row = "<td>$period</td>";
+    my $sum = 0;
+    foreach my $title ( @titles ) {
+        if ( $data->{ $period }->{ $title } ) {
+            $total->{ $title } += $data->{ $period }->{ $title };
+            $sum += $data->{ $period }->{ $title };
+        }
+        my $diff = $data->{ $period }->{ $title } || '';
+        $diff = $diff ? sprintf("%.2f", $diff) : '';
+        $table_row .= "<td>$diff</td>";
+    }
+    $sum = sprintf("%.2f", $sum);
+    $table_row .= "<td><b>$sum</b></td>";
+    $table .= "<tr>$table_row</tr>";
 }
 my $table_row = '<td><b>Total</b></td>';
 my $sum = 0;
 foreach my $title ( sort keys %{ $titles } ) {
-	my $diff = '';
-	$diff = sprintf("%.2f", $total->{ $title }) if $total->{ $title };
-	$table_row .= "<td><b>$diff</b></td>";
-	$sum += $diff;
+    my $diff = '';
+    $diff = sprintf("%.2f", $total->{ $title }) if $total->{ $title };
+    $table_row .= "<td><b>$diff</b></td>";
+    $sum += $diff;
 }
 $sum = sprintf("%.2f", $sum);
 $table_row .= "<td><b>$sum</b></td>";
@@ -63,11 +64,11 @@ $table .= '</table>';
 my $sample = qq{
 <!DOCTYPE html>
 <html>
-	<head><title>Balance</title></head>
-	<body>
-		<h2>Detailed balance</h2>
-		<div><font size="1">$table</font></div>
-	</body>
+    <head><title>Balance</title></head>
+    <body>
+        <h2>Detailed balance</h2>
+        <div><font size="1">$table</font></div>
+    </body>
 </html>
 };
 
